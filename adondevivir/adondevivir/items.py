a0c9ba16cc519725ae4c3343fcd2f98cedbf9348
José Chu <jchudb93@gmail.com>
2019-02-28 15:38:37 -0500

    spiders

--- adondevivir/adondevivir/items.py
+++ adondevivir/adondevivir/items.py
@@ -1,12 +1,7 @@
-# -*- coding: utf-8 -*-
-
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
 from scrapy.loader import ItemLoader
+
 
 class PredioItem(scrapy.Item):
     name = scrapy.Field()
@@ -15,7 +10,6 @@ class PredioItem(scrapy.Item):
 
     url = scrapy.Field()
 
+
 class PredioItemLoader(ItemLoader):
     default_item_class = PredioItem
-    
-
--- adondevivir/adondevivir/spiders/adondevivir.py
+++ adondevivir/adondevivir/spiders/adondevivir.py
@@ -3,5 +3,13 @@ from adondevivir.items import PredioItemLoader
 
 class AdondeVivirSpider(Spider):
     name = 'adondevivir'
-    start_urls = []
-    
+    start_urls = [
+      'https://www.adondevivir.com/propiedades/flat-petit-tower-53931295.html'
+    ]
+
+    def parse(self, response):
+        pl = PredioItemLoader(response=response)
+        pl.add_value('url', response.url)
+        pl.add_xpath('name', '//h2[has-class("info-title")]')
+        pl.add_xpath('address', '//h2[has-class("info-location")]')
+        yield pl.load_item()        
