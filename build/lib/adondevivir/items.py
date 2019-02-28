f6425f2a1021429726bfd9ee5ddc29bf8b7f4740
José Chu <jchudb93@gmail.com>
2019-02-28 16:16:48 -0500

    asdf

---
+++ build/lib/adondevivir/items.py
@@ -0,0 +1,23 @@
+
+import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst, MapCompose, Join
+
+from w3lib.html import remove_tags
+
+
+class PredioItem(scrapy.Item):
+    name = scrapy.Field()
+    address = scrapy.Field()
+    description = scrapy.Field()
+    price = scrapy.Field()
+
+    url = scrapy.Field()
+
+
+class PredioItemLoader(ItemLoader):
+    default_item_class = PredioItem
+    default_output_processor = TakeFirst()
+    default_input_processor = MapCompose(remove_tags, str.strip)
+
+    description_out = Join('\n')
