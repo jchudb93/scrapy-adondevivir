f5c443af6e53fcfe7b13c613061c340da96e9d9e
José Chu <jchudb93@gmail.com>
2019-02-28 17:58:16 -0500

    asdf

--- adondevivir/spiders/adondevivir.py
+++ adondevivir/spiders/adondevivir.py
@@ -8,17 +8,19 @@ class AdondeVivirSpider(Spider):
     ]
 
     def parse(self, response):
-        item = response.xpath('//h4[has-class("aviso-data-title")/a')
+        items = response.xpath('//h4[has-class("aviso-data-title")]/a')
         for item in items:
-            url = item.xpath("./@href").extract_first(default='')
+            url = item.xpath('./@href').extract_first(default='')
             url = response.urljoin(url)
             yield Request(url=url, callback=self.parse_predio)
 
     def parse_predio(self, response):
         pl = PredioItemLoader(response=response)
+
         pl.add_value('url', response.url)
         pl.add_xpath('name', '//h2[has-class("info-title")]')
         pl.add_xpath('address', '//h2[has-class("info-location")]')
         pl.add_xpath('description', '//div[has-class("description-container")]')
         pl.add_xpath('price', '//span[has-class("data-price")]')
-        yield pl.load_item()        
+
+        yield pl.load_item()       
