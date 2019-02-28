4e108696fa91cc9c6f3c638dbe5aa37ef3e3dacc
José Chu <jchudb93@gmail.com>
2019-02-28 16:05:32 -0500

    shub

---
+++ adondevivir/build/lib/adondevivir/spiders/adondevivir.py
@@ -0,0 +1,17 @@
+from scrapy import Spider
+from adondevivir.items import PredioItemLoader
+
+class AdondeVivirSpider(Spider):
+    name = 'adondevivir'
+    start_urls = [
+      'https://www.adondevivir.com/propiedades/flat-petit-tower-53931295.html'
+    ]
+
+    def parse(self, response):
+        pl = PredioItemLoader(response=response)
+        pl.add_value('url', response.url)
+        pl.add_xpath('name', '//h2[has-class("info-title")]')
+        pl.add_xpath('address', '//h2[has-class("info-location")]')
+        pl.add_xpath('description', '//div[has-class("description-container")]')
+        pl.add_xpath('price', '//span[has-class("data-price")]')
+        yield pl.load_item()        
